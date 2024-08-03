0ca7b7b57385187a79a4d4c7e5ff23d341ad6d91
MyShoesHaveSouls <99381059+MyShoesHaveSouls@users.noreply.github.com>
2024-08-03 04:15:06 -0600

    Update script.py

--- script.py
+++ script.py
@@ -2,9 +2,10 @@ import threading
 import requests
 from eth_account import Account
 from queue import Queue
+import itertools
 
-# Etherscan API key (replace with your actual API key)
-API_KEYS  = [
+# Etherscan API keys (replace with your actual API keys)
+API_KEYS = [
     'F92Z14GE2DTF6PBBYY1YPHPJ438PT3P2VI',
     '4Q5U7HNF4CGTVTGEMGRV5ZU9WYNJ6N7YA5',
     'EX8K12JY7BCVG8RAUU8X2Z6QT2GCF5EYB4',
@@ -24,37 +25,39 @@ def generate_address(private_key_int):
     return account.address, private_key
 
 # Function to check the balance of an Ethereum address using Etherscan API
-def check_balance(address):
-    url = f"https://api.etherscan.io/api?module=account&action=balance&address={address}&tag=latest&apikey={API_KEYS}"
+def check_balance(address, api_key):
+    url = f"https://api.etherscan.io/api?module=account&action=balance&address={address}&tag=latest&apikey={api_key}"
     response = requests.get(url)
     data = response.json()
     if data['status'] == '1':
         return int(data['result']) / 1e18  # Convert wei to ether
     return 0
 
-# Worker function for threading
 # Minimum balance threshold in ETH for printing
 MIN_BALANCE = 0.0  # Set to desired value, e.g., 0.1 ETH for a minimum balance of 0.1 ETH
 
 # Worker function for threading
-def worker(queue):
+def worker(queue, api_keys_iter):
     while not queue.empty():
         private_key_int = queue.get()
         address, private_key = generate_address(private_key_int)
-        balance = check_balance(address)
+        api_key = next(api_keys_iter)
+        balance = check_balance(address, api_key)
         if balance > MIN_BALANCE:
             print(f'Match found! Address: {address}, Private Key: {private_key}, Balance: {balance} ETH')
         queue.task_done()
-
 
 def main(start, end, num_threads):
     queue = Queue()
     for i in range(start, end):
         queue.put(i)
     
+    # Create an infinite iterator for API keys to cycle through them
+    api_keys_iter = itertools.cycle(API_KEYS)
+
     threads = []
     for _ in range(num_threads):
-        t = threading.Thread(target=worker, args=(queue,))
+        t = threading.Thread(target=worker, args=(queue, api_keys_iter))
         t.start()
         threads.append(t)
     
